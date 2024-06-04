7de9db8d39c08237c896c9bbfb6d20e4bc456671
BoyaSadhguru <boyasadhguru@gmail.com>
2024-06-04 12:35:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="sadguru";
+names[1]="murali";
+names[2]="prem";
+names[3]="hari";
+names[4]="akhil";
+names[5]="srikanth";
+names[6]="sathish";
+names[7]="vinay";
+names[8]="suneo";
+names[9]="ramana";
 
 
 for (var i = 0; i < names.length; i++) {
